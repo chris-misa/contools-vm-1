649c400fff6e0a7a0e657e5861309d87ca3338c3
tank <chris@chrismisa.com>
2018-08-06 14:35:59 -0700

    added unique name on each interface

--- profile.py
+++ profile.py
@@ -37,7 +37,7 @@ for cores in CORES:
     newNode = request.XenVM(newNodeName)
     newNode.cores = cores
     newNode.ram = ram
-    newIface = newNode.addInterface("if1")
+    newIface = newNode.addInterface("if" + str(ipCount))
     newIface.component_id = "eth1"
     newIface.addAddress(rspec.IPv4Address("10.10.1."+str(ipCount),"255.255.255.0"))
     ipCount += 1
